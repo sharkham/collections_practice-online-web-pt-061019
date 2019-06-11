70122207ea482a009432e8c25e85dcf8ef66ba60
Sam Markham <sam.e.markham@gmail.com>
2019-06-11 18:32:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,7 +24,7 @@ def swap_elements(array)
 end 
 
 def swap_elements_from_to(array, index, destination_index)
-  array[index] = array[destination_index]
+  array[index], array[destination_index] = array[destination_index], array[index]
   return array 
 end 
 binding.pry 
