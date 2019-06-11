55e0f42973874f3f7957259c898648f1c27c6b42
Sam Markham <sam.e.markham@gmail.com>
2019-06-11 18:06:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,7 +52,9 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    unless index == 1 
+    if index == 1 
+      element 
+    else 
       element + "s"
     end 
   end 
