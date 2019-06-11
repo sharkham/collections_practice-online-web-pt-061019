28405f8ee8df8d9b82fc7890778273a95dcab1cd
Sam Markham <sam.e.markham@gmail.com>
2019-06-11 18:09:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,8 +25,12 @@ def swap_elements(array)
   array.each_with_index.collect do |item, index|
     if index == 1 
       index + 1 
+      item 
     elsif index == 2 
       index - 1 
+      item 
+    else 
+      item 
     end 
   end 
 end 
