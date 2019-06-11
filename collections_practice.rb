9e807cdaa6e5bbce30a882cd290afa90a7fd0576
Sam Markham <sam.e.markham@gmail.com>
2019-06-11 17:45:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end 
+end 
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a 
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b| 
+    a.length <=> b.length 
+  end 
+end 
+
+# def swap_elements(array)
+#   array[1] <=> array[2]
+#   return array
+# end 
+
+def swap_elements(array)
+  array.each_with_index.collect do |item, index|
+    
+  end 
+end 
+
+def reverse_array(array)
+  new_array = array.reverse
+end 
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |word|
+    kesha_array << word[2] = "$"
+  end 
+end 
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end 
+end 
+
+def sum_array(array)
+  
+end 
